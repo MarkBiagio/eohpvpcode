91daf6341a41637bf4712f611d905801ae3e18f3
MarkBiagio <markbiagio@gmail.com>
2016-02-20 14:48:01 +0200

    added gul task to deploy to api project for running

--- PVP/src/PVP.WebClient2/gulpfile.js
+++ PVP/src/PVP.WebClient2/gulpfile.js
@@ -4,7 +4,45 @@ Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
 */
 
 var gulp = require('gulp');
+var debug = require('gulp-debug'); //logging
+var del = require('del');
+var runSequence = require('run-sequence');
 
-gulp.task('default', function () {
-    // place code for your default task here
+var deployFolder = '../PVP.WebApi/wwwroot';
+
+gulp.task('build', function (done) {
+    runSequence('cleanServer',
+                'copyClientToServer',
+             function () {
+                 console.log('.. all done ..');
+                 done();
+             });
+});
+
+gulp.task('cleanServer', function () {
+
+    return del([
+        '../PVP.WebApi/wwwroot/app',
+        '../PVP.WebApi/wwwroot/scripts',
+        '../PVP.WebApi/wwwroot/index.html'
+
+    ], { force: true });;
+    //return gulp.src(
+    //  [
+         
+    //  ], { read: false }) // much faster
+    //      .pipe(debug({ title: 'deleteing:' }))
+    //      .pipe(rimraf({ force: true }));
+
+});
+
+gulp.task('copyClientToServer', function () {
+    return gulp
+       .src([
+           'app/**/*',  
+           'scripts/**/*',
+           'index.html'
+       ], { base: '.' })
+        .pipe(debug({ title: 'copying:' }))
+		.pipe(gulp.dest(deployFolder));
 });
