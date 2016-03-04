5980d63c990842c70bd427c6f99f4b12ded21a01
Adriaan Putter <adriaan.putter@gmail.com>
2016-03-04 22:18:06 +0200

    make pragmatic changes skipping BLL/Service layer

--- PVP/src/PVP.DAL/UserRepository.cs
+++ PVP/src/PVP.DAL/UserRepository.cs
@@ -1,102 +1,23 @@
 ﻿using PVP.DomainModel;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using PVP.DAL.DBModel;
-using System.Data.SqlClient;
+using Simple.Data;
 
 namespace PVP.DAL
 {
     public class UserRepository
     {
-        private const string CONNECTION_STRING = "Data Source=tcp:ad16o7zdca.database.windows.net;" +
-                "Initial Catalog=PVP.SQL;" +
-                "MultipleActiveResultSets=True;" +
-                "User Id=EOHStaging; Password=y@l3b123;";
-
-        public UserRepository()
+        public List<User> GetUsers()
         {
-
+            var db = Database.Open();
+            List<User> users = db.tbUser.FindAll();
+            return users;
         }
 
-        public List<tbUser> GetUsers()
+        public User GetUser(int userId)
         {
-            List<tbUser> tbUsers = new List<tbUser>();
-
-            using (SqlConnection cnn =  new SqlConnection(CONNECTION_STRING))
-            {
-                SqlCommand command = new SqlCommand("prUser_GetUsers", cnn);
-                command.CommandType = System.Data.CommandType.StoredProcedure;
-                cnn.Open();
-
-                SqlDataReader reader = command.ExecuteReader();
-
-                if (reader.HasRows)
-                {
-                    while (reader.Read())
-                    {
-                        tbUsers.Add(new tbUser()
-                        {
-                            UserId = reader.GetInt32(0),
-                            LoginName = reader.GetString(1),
-                            Name = reader.GetString(2),
-                            IsActive = reader.GetBoolean(3)
-                        });
-                        
-                    }
-                }
-                
-            }
-            return tbUsers;
-            //return new List<tbUser> {
-            //    new tbUser() { UserId = 1, LoginName = "mark@eoh.co.za", Name= "Mark", IsActive=true },
-            //    new tbUser() { UserId = 2, LoginName = "adriaan@eoh.co.za", Name= "Adriaan" , IsActive=true},
-            //};
-        }
-
-        public tbUser GetUser(int userId)
-        {
-            return new tbUser() { UserId = 1, LoginName = "mark@eoh.co.za", Name = "Mark", IsActive = true };
-                
-        }
-
-        public List<tbRole> GetUserRoles(int userId)
-        {
-            List<tbRole> tbRoles = new List<tbRole>();
-
-            using (SqlConnection cnn = new SqlConnection(CONNECTION_STRING))
-            {
-                SqlCommand command = new SqlCommand("prRole_GetRoleForUser", cnn);
-                command.CommandType = System.Data.CommandType.StoredProcedure;
-                SqlParameter p = new SqlParameter("@UserId", System.Data.SqlDbType.Int);
-                p.Value = userId;
-                command.Parameters.Add(p);
-                cnn.Open();
-
-                SqlDataReader reader = command.ExecuteReader();
-
-                if (reader.HasRows)
-                {
-                    while (reader.Read())
-                    {
-                        tbRoles.Add(new tbRole()
-                        {
-                            RoleId = reader.GetInt32(0),
-                            Description = reader.GetString(1)
-                        });
-
-                    }
-                }
-
-                
-            }
-            return tbRoles;
-
-            //return new List<tbRole> {
-            //    new tbRole() { RoleId = 1, Description = "User"},
-            //    new tbRole() { RoleId = 2, Description = "Administrator"},
-            //};
+            var db = Database.Open();
+            User user = db.tbUser.Get(userId);
+            return user;
         }
     }
 }
--- PVP/src/PVP.WebApi/Controllers/UsersController.cs
+++ PVP/src/PVP.WebApi/Controllers/UsersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using PVP.DAL;
 using PVP.DomainModel;
 
 
@@ -11,20 +12,23 @@ namespace PVP.WebApi.Controllers
     [Route("api/[controller]")]
     public class UsersController : Controller
     {
+        private UserRepository _repository;
+        
+        public UsersController()
+        {
+            _repository = new UserRepository();
+        }
+        
         [HttpGet]
         public IEnumerable<User> Get()
         {
-            BLL.UserBLL bll = new BLL.UserBLL();
-            return bll.GetUsers();
-
+            return _repository.GetUsers();
         }
 
         [HttpGet("{id}")]
         public User Get(int id)
         {
-            BLL.UserBLL bll = new BLL.UserBLL();
-            return bll.GetUser(1);
-               
+            return _repository.GetUser(id);
         }
 
     }
